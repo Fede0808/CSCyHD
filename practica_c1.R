dc01e36deb535d840e93079726611817bbf0fb0d
fblasco1 <francisco.o.blasco@gmail.com>
2023-05-11 17:57:09 -0300

    Initial Commit

---
+++ practica_c1.R
@@ -0,0 +1,33 @@
+"mi_numero" <- 8
+"otro_numero" <- mi_numero+4
+otro_numero > 10
+(otro_numero > 10 | mi_numero > 10)
+cosa <- 5*6
+
+
+### PRACTICA TIPO DE OBJETOS ####
+
+VECTNUM <- c(1,2,10,20,30,40,50)
+
+VECTNUM [1:7]<25
+
+VECTNUM [c(3,5)]   
+
+MENORES_25 <- c(VECTNUM < 25) 
+
+
+
+### PRACTICA IRIS ####
+
+VECTOR0 <- c(10,20,30)
+VECTOR0 [2] <- 40
+VECTOR1 <- c("Hola","Como","Estas")
+VECTOR2 <- c("salud","dinero","amor")
+VECTOR3 <- c("tradicion","familia","propiedad")
+DATAF <- data.frame(VECTOR0,VECTOR1,VECTOR2)
+OTRA_COSA <- DATAF [2,3]
+
+
+
+
+
