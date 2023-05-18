d1a4210641404b5a90d72076904074456839b986
Federico Blasco <federicoblasco8@gmail.com>
2023-05-18 11:37:03 -0300

    Resuelto el ejercicio 1 y en proceso el ejercicio 2. Problemas con las variables porque trae PONDIIO, revisar diseno de encuesta.

--- Clase 1 practica eph.R
+++ Clase 1 practica eph.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(eph)
 
+
 # EJERCICIO 1 ####
 
 baset117 <- get_microdata(year = 2017,trimester = 1)
@@ -9,7 +10,22 @@ mis_variables <- c("ANO4","REGION","CH06","ESTADO")
 
 basesita <- baset117[mis_variables]
 
-basesita_100 #¿Como se restringe la muestra a los 100 primeros casos?
+basesita_100 <- baset117 [1:100,4] #¿Como se restringe la muestra a los 100 primeros casos? 
 
-pruebas_serias <- sample(basesita,size =10000 ,replace=FALSE,prob=NULL)
+pruebas_serias <- sample(x=baset117,size=10000 ,replace=TRUE,prob=NULL)
+
+# EJERCICIO 2 ####
+
+variables_m30 <- c("CODUSU","NRO_HOGAR","COMPONENTE","AGLOMERADO","CH04","CH06","P21")
+
+# base_m30 <- get_microdata(year = c(2015:2022),trimester= c(1:4),type="individual",vars = variables_m30) ¿porque no funciona?
+
+base_2015 <- get_microdata(year = 2015,trimester= 1)
+
+base_m30 <- base_2015 [variables_m30]
+
+base_m30s <- subset(base_m30,CH06<30)
+
+saveRDS(base_m30s,file = "base h30")
+
 
